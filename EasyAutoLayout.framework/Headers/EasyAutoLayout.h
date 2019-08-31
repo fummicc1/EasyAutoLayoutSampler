705ec483d1e0fb4dc584af24a03164c70d1cc7e1
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-31 21:10:08 +0900

    commit

---
+++ EasyAutoLayout.framework/Headers/EasyAutoLayout.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyAutoLayout.
+FOUNDATION_EXPORT double EasyAutoLayoutVersionNumber;
+
+//! Project version string for EasyAutoLayout.
+FOUNDATION_EXPORT const unsigned char EasyAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyAutoLayout/PublicHeader.h>
--- EasyAutoLayoutSampler/ViewController.swift
+++ EasyAutoLayoutSampler/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import EasyAutoLayout
 
-class ViewController: UIViewController {
+class ViewController: EasyAutoLayoutViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var dataList: [String] = [
+        "気分は晴れ",
+        "天気も晴れだといいな。",
+        "Swift",
+        "競技プログラミングやってみたい"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        tableViewCell.textLabel?.text = dataList[indexPath.row]
+        return tableViewCell
+    }
+}

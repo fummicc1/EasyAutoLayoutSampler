efe617a561c1cbba877ce69dd5c044d8de147255
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-31 21:00:29 +0900

    Initial Commit

---
+++ EasyAutoLayoutSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyAutoLayoutSampler
+//
+//  Created by Fumiya Tanaka on 2019/08/31.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
